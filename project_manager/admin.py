021e57be65f58991ab0f39e736b0f1d18974dbf3
odonnellryanc <odonnellryanc@gmail.com>
2015-03-11 14:53:23 -0400

    registered models for admin page

--- project_manager/admin.py
+++ project_manager/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from project_manager.models import Member, Project, WorkItem, TimeEstimate, Role
+
+admin.site.register((Member, Project, WorkItem, TimeEstimate, Role))
 
 # Register your models here.
